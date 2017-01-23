f6d410274b490dd3931ffcb62834c20e68774708
Matthew Prout <mdprout88@gmail.com>
2017-01-23 11:06:16 -0500

    added bubble_sort_by method

--- bubble.rb
+++ bubble.rb
@@ -14,3 +14,18 @@ puppy = [1,4,2,8,5,3,12,10]
             end 
         puts array
     end
+
+ def bubble_sort_by(array)
+        n = array.length
+        swapped = true
+            until swapped == false do
+                swapped = false
+                for i in 1..n-1 do
+                    if (yield array[i-1],array[i]) > 0
+                        array[i-1], array[i] = array[i], array[i-1]
+                        swapped = true
+                    end
+                end
+            end 
+        puts array
+    end
